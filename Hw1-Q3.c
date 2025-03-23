75d38f4d4a47bf2f13842609e31452a7d19fab1e
Kadir Mehmet Ağır <kadiragir@hacettepe.edu.tr>
2025-03-23 16:45:50 +0300

    Add files via upload

---
+++ Hw1-Q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+  int homeValue;
+  int popularity;
+  int size;
+  
+  printf("Enter popularity\n");
+  scanf("%d", &popularity);
+  
+  printf("Enter size\n");
+  scanf("%d", &size);
+  
+  homeValue = (pow(popularity, 3) + pow(size, 2)) * 10000;
+  
+  printf("Home Value is %d\n", homeValue);
+  
+  getchar();	
+  getchar();
+  
+  return 0;
+}
